a146524de308057403d3c1d6b1bc5b8e02b60a80
massaracsh7 <fatum7@gmail.com>
2024-10-26 22:24:49 +0300

    refactor: update styles structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,21 +5,21 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <nav className={styles.navigation}>
-        <ul className={styles.navList}>
-          <li>
-            <a href="#hero" aria-label="About me">Who am I?</a>
+        <ul className={styles.navigation__list}>
+          <li className={styles.navigation__item}>
+            <a href="#hero" className={styles.navigation__link} aria-label="About me">Who am I?</a>
           </li>
-          <li>
-            <a href="#projects" aria-label="My projects">What I've Built</a>
+          <li className={styles.navigation__item}>
+            <a href="#projects" className={styles.navigation__link} aria-label="My projects">What I've Built</a>
           </li>
-          <li>
-            <a href="#contacts" aria-label="Contact me">Let's Connect</a>
+          <li className={styles.navigation__item}>
+            <a href="#contacts" className={styles.navigation__link} aria-label="Contact me">Let's Connect</a>
           </li>
-          <li>
-            <a href="#skills" aria-label="My skills">My Toolbox</a>
+          <li className={styles.navigation__item}>
+            <a href="#skills" className={styles.navigation__link} aria-label="My skills">My Toolbox</a>
           </li>
-          <li>
-            <a href="#education" aria-label="My education">My Learning Journey</a>
+          <li className={styles.navigation__item}>
+            <a href="#education" className={styles.navigation__link} aria-label="My education">My Learning Journey</a>
           </li>
         </ul>
       </nav>
